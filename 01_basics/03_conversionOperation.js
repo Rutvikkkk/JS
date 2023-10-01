6fe55fb34d5543f6db722ee79f0cd08df52c4d62
Rutvikkkk <rutvikbhil44@gmail.com>
2023-10-01 18:20:49 +0530

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,7 +28,23 @@
 
     let stringNumber = String(someNumber)
 
-    console.log(stringNumber);
-    console.log(typeof stringNumber);
+    // console.log(stringNumber);
+    // console.log(typeof stringNumber);
+
+    let value = 3
+    let negValue = -value
+    // console.log(negValue);
     
+    let str1 = "Hello"
+    let str2 = " Rutvik"
+
+    let str3 = str1 + str2
     
+    console.log(str3);
+
+    console.log("1" + 2);
+    console.log(1 + "2");
+    console.log("1" + 2  + 2);
+    console.log(2 + 2 + "1");
+
+    console.log(4 + 4 * 3 / 2 - 10);
