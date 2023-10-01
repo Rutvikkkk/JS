83bee110e8cf38371f5020c027927ae646b7e627
Rutvikkkk <rutvikbhil44@gmail.com>
2023-10-02 00:37:38 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+
+//console.log("2" == 2);
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);             //this type of copmarison are tricky 
+
+
+
+// ===
+
+
+console.log("2" === 2);            // if there only == then it's true
